98dad096f54de7d2955a723ffb529b0c97a2cd48
harshghatode <harshghatode1000@gmail.com>
2025-02-12 03:29:31 +0530

    Day 10

--- SBA_With_Development/src/main/java/com/example/demo/SbaWithDevelopmentApplication.java
+++ SBA_With_Development/src/main/java/com/example/demo/SbaWithDevelopmentApplication.java
@@ -8,6 +8,10 @@ public class SbaWithDevelopmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbaWithDevelopmentApplication.class, args);
+		System.out.println("Dev tools test 1");
+		System.out.println("Dev tools test 2");
+		System.out.println("Dev tools test 3");
+		
 	}
 
 }
